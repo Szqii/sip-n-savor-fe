ce2f2f9caedefd58ac80f2d09f607d6e34be854f
Szqii <szqii.altn@gmail.com>
2023-10-30 12:37:24 +0300

    create: item detail

---
+++ src/components/SingleCoffeeItem.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type {Coffee} from "@/types/coffee";
+
+defineProps({
+  coffee: {
+    type: Object as () => Coffee,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="max-w-sm rounded-lg overflow-hidden bg-primary">
+    <img class="w-full" :src="coffee.imageUrl" :alt="coffee.name">
+    <div class="px-6 py-4">
+      <div class="font-bold text-white text-xl mb-2">{{ coffee.name }}</div>
+      <p class="text-gray-700 text-base">{{ coffee.longDescription }}</p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+            v-for="(tag, index) in coffee.tags"
+            :key="index">{{ tag }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/SingleItemView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import axios from "axios";
+import router from "@/router";
+import type {Coffee} from "@/types/coffee";
+import SingleCoffeeItem from "@/components/SingleCoffeeItem.vue";
+
+const itemId = router.currentRoute.value.params.id
+const coffee = ref({} as Coffee)
+
+onMounted(() => {
+  axios.get(`http://localhost:3000/api/get-coffee/${itemId}`)
+      .then((response) => {
+        coffee.value = response.data.coffee as Coffee
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+})
+</script>
+
+<template>
+  <div class="container mx-auto items-center justify-center flex my-auto">
+    <single-coffee-item :coffee="coffee"/>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
